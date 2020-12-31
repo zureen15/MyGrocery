a2f804c9db1f6a7768c00d99b78eec4f24d61471
potatogerms <luqman13_rider@yahoo.com>
2020-12-31 15:47:30 +0800

    Update auth.service.ts

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { off } from 'process';
+import {BehaviorSubject, Observable, of } from 'rxjs';
+import { IonicStorageModule} from '@ionic/storage' ; 
+
+const TOKEN_KEY = 'user-access-token'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user : Observable <any>;
+ private authState = new BehaviorSubject (null) ;
+
+  constructor(private storage: Storage) { 
+    this.user =  this.authState.asObservable();
+  }
+
+
+    signIn (credentials) : Observable<any> {
+      let email = credentials.email;
+      let password = credentials.password;
+      let user = null;
+
+      if ( email === 'admin' && password === 'admin') {
+        user = {email, role: 'ADMIN'} ;
+      } else user = { email, role: 'CUSTOMER'};
+
+      this.authState.next(user);
+
+      this.storage.set( TOKEN_KEY,user );
+
+      return of(user);
+
+    }
+     
+}
