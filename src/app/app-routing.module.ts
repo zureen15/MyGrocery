91f9b619d16a2c622a1434620c0450a5c724b2c7
potatogerms <luqman13_rider@yahoo.com>
2021-01-04 19:07:50 +0800

    routing
    first page tukar ke seller type

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'user-type',
     pathMatch: 'full'
   },
   {
--- src/app/login-seller/login-seller.page.ts
+++ src/app/login-seller/login-seller.page.ts
@@ -12,12 +12,12 @@ import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 export class LoginSellerPage implements OnInit {
 
   user = {
-    username : '',
+    
     email: '',
    password: ''
 }
 
-  constructor(private router: Router,
+  constructor(private router: Router, private faio : FingerprintAIO,
     public ngFireAuth: AngularFireAuth) { }
 
   ngOnInit() {
@@ -32,5 +32,27 @@ export class LoginSellerPage implements OnInit {
         alert ('login failed!');
       }
   }
+
+  logIn() {
+    this.faio.show({
+     cancelButtonTitle: 'Cancel',
+     title: 'Biometric Authentication', // (Android Only) | optional | Default: "<APP_NAME> Biometric Sign On"
+     subtitle: 'Coolest Plugin ever',// (Android Only) | optional | Default: null
+     description: 'Please authenticate', // optional | Default: null
+     fallbackButtonTitle: 'Use Backup', // optional | When disableBackup is false defaults to "Use Pin".
+                                        // When disableBackup is true defaults to "Cancel"
+     disableBackup:true,  // optional | default: false
+   })
+   .then((result: any) =>{ console.log(result)
+       alert("Successfully Authenticated!")
+       this.router.navigate(['/home-seller']);
+   })
+   
+   .catch((error: any) =>{ console.log(error)
+        alert ("Match not found!")
+        
+       });
+   
+    }
 
 }
